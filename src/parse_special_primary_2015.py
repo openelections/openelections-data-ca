6fd9266d9c78ced68cdf3baae041772a85b2f7d5
Charles DiFazio <charles@difaz.io>
2017-11-06 23:26:18 -0800

    San Bernardino county precinct-level results for 2015 Special Primary
     - Parse SoV and write-ins separately
     - Fixes #72

--- src/parse_special_primary_2015.py
+++ src/parse_special_primary_2015.py
@@ -5,6 +5,8 @@ import tempfile
 import zipfile
 import PyPDF2
 
+from tabula import read_pdf
+
 candidates = {'JOHN M. W. MOORLACH': 'John M. W. Moorlach',
               'DONALD P. WAGNER': 'Donald P. Wagner',
               'NAZ NAMAZI': 'Naz Namazi',
@@ -23,10 +25,16 @@ p = {'Sharon Runner':      'REP',
      'Susan Bonilla':      'DEM',
      'Joan Buchanan':      'DEM',
      'Michaela M. Hertle': 'REP',
-     'Steve Glazer':       'DEM'}
+     'Steve Glazer':       'DEM',
+     'Joshua Conaway':     'DEM',
+     'Steve Hill':         'DEM',
+     'Richard E. Macias':  'DEM',
+     'Jerry J. Laws':      'REP',
+     'Joshua C. Chandler': 'NPP',
+     'Jason Zink':         'NPP'}
 
 
-def prepare_output(df, county, cand):
+def prepare_output(df, county, district, cand):
     df.columns = ['precinct'] + cand
     df = pd.melt(df, id_vars='precinct',
                  value_vars=cand, var_name='candidate', value_name='votes').dropna()
@@ -34,7 +42,7 @@ def prepare_output(df, county, cand):
 
     df = df.assign(county=county,
                    office='State Senate',
-                   district=7)
+                   district=district)
     df.votes = df.votes.astype(int)
 
     df = df.groupby(
@@ -50,7 +58,7 @@ def parse_alameda():
     primary = primary.loc[(primary.index > 3) & (primary.index < 385)][
         ['Alameda County', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12']]
 
-    table = prepare_output(primary, 'Alameda',
+    table = prepare_output(primary, 'Alameda', 7,
                            ['Terry Kremin', 'Susan Bonilla', 'Joan Buchanan', 'Michaela M. Hertle', 'Steve Glazer'])
     for x in ['candidate', 'district', 'office', 'precinct', 'county']:
         table = table.sort_values(by=x, kind='mergesort')
@@ -64,7 +72,7 @@ def parse_contra_costa():
     primary = primary.loc[(primary.index > 2) & (primary.index < 458)][
         ['Return to table of content', 'Unnamed: 4', 'Unnamed: 7', 'Unnamed: 10', 'Unnamed: 13', 'Unnamed: 16']]
 
-    table = prepare_output(primary, 'Contra Costa',
+    table = prepare_output(primary, 'Contra Costa', 7,
                            ['Terry Kremin', 'Susan Bonilla', 'Joan Buchanan', 'Michaela M. Hertle', 'Steve Glazer'])
     for x in ['candidate', 'district', 'office', 'precinct', 'county']:
         table = table.sort_values(by=x, kind='mergesort')
@@ -143,11 +151,67 @@ def parse_los_angeles():
         '2015/20150317__ca__special__primary__los_angeles__precinct.csv', index=False)
 
 
+def san_bernardino_sovc():
+    sovc_url = 'http://www.sbcountyelections.com/Portals/9/Elections/2015/0317/SOV%20Book.pdf'
+    primary = read_pdf(sovc_url, pages="1-6",
+                       pandas_options={'error_bad_lines': False})
+    first = primary.loc[((primary.index > 2) & (primary.index < 64))][
+        ['Unnamed: 0', 'Unnamed: 5']].rename(columns={'Unnamed: 5': 'votes'})
+    second = primary.loc[((primary.index > 67) & (primary.index < 129)) |
+                         ((primary.index > 132) & (primary.index < 194)) |
+                         ((primary.index > 197) & (primary.index < 243))][
+        ['Unnamed: 0', 'Unnamed: 4']].rename(columns={'Unnamed: 4': 'votes'})
+
+    primary = read_pdf(sovc_url, pages="3,5",
+                       pandas_options={'error_bad_lines': False})
+    third = primary.loc[primary.index > 2][
+        ['Unnamed: 0', 'Unnamed: 5']].rename(columns={'Unnamed: 5': 'votes'})
+
+    pcand = 'SHARON RUNNER'
+    merged = first.append(second).append(third)
+    merged['precinct'] = merged['Unnamed: 0'].apply(
+        lambda x: x[len(pcand):][:7] if str(
+            x).startswith(pcand) else str(x)[:7])
+
+    return prepare_output(merged[['precinct', 'votes']],
+                          'San Bernardino', 21,
+                          ['Sharon Runner'])
+
+
+def san_bernardino_writeins():
+    writeins_url = 'http://www.sbcountyelections.com/Portals/9/Elections/2015/0317/PublicNotice_QualifiedWrite-InResults_FINAL.pdf'
+    writeins_req = requests.get(writeins_url)
+    writeins_req.raise_for_status()
+    writeins_pdf = PyPDF2.PdfFileReader(io.BytesIO(writeins_req.content))
+    cands = writeins_pdf.getPage(0).extractText().split('\n')[35:70:2]
+    return [{'candidate': c, 'votes': int(v), 'PRECINCT': 'Write-In'} for
+            c, v in zip(cands[::3], cands[2::3])]
+
+
+def parse_san_bernardino():
+    output_columns = ['county', 'precinct', 'office',
+                      'district', 'party', 'candidate', 'votes']
+
+    table = san_bernardino_sovc().append(san_bernardino_writeins())
+    table = table.assign(county='San Bernardino', office='State Senate',
+                         district='21').replace({'candidate': candidates})
+
+    parties = {k: 'W/I' for k in candidates.values()}
+    parties.update(p)
+
+    table['party'] = table.candidate.apply(lambda x: parties[x])
+    for x in ['candidate', 'district', 'office', 'precinct', 'county']:
+        table = table.sort_values(by=x, kind='mergesort')
+    table[output_columns].to_csv(
+        '2015/20150317__ca__special__primary__san_bernardino__precinct.csv', index=False)
+
+
 def main():
     parse_alameda()
     parse_contra_costa()
     parse_orange()
     parse_los_angeles()
+    parse_san_bernardino()
 
 if __name__ == "__main__":
     main()
