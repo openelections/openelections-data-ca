c6e72b61239e8c030dfe9b7d7508e571f9fb96a3
Kenneth Hsu <kennethhsu@gmail.com>
2021-04-05 16:38:56 -0700

    Add test for empty columns.

--- tests/test_data.py
+++ tests/test_data.py
@@ -62,9 +62,14 @@ class FileFormatTests(unittest.TestCase):
 
                                 required_headers = set(FileFormatTests.__get_expected_headers(file))
                                 headers = set(next(reader))
+                                # Verify that the header does not contain any empty entries.
+                                self.assertNotIn("", headers, f"File {csv_file} has an empty column header.")
+
+                                # Verify that the header contains the required entries.
                                 self.assertTrue(required_headers.issubset(headers), f"File {csv_file} has "
                                     f"header: {headers}, which is missing: {required_headers.difference(headers)}.")
 
+                                # Verify that each row has the expected number of entries.
                                 num_headers = len(headers)
                                 for row in reader:
                                     self.assertEqual(num_headers, len(row), f"File {csv_file} has header {headers}, but row {row}.")
