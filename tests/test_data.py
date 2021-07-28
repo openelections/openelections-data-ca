71af0d40474d3f7728e4a090786db8082bace105
Kenneth Hsu <kennethhsu@gmail.com>
2021-07-28 12:46:54 -0700

    Fix path to county files in tests.
    The 2016 county files were moved in b052d555b0 ("statewide general file
    & moved to counties folder"), which prevented the tests from running on
    these files.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import glob
+import os
 import pandas
 import pytest
 
@@ -8,25 +9,26 @@ import pytest
 columns = ['candidate', 'county', 'district', 'office', 'votes']
 
 
-@pytest.mark.parametrize("year,date,election_type", [
-    ("2014", "0603", "primary"),
-    ("2014", "1104", "general"),
-    ("2015", "0317", "special__primary"),
-    ("2015", "0519", "special__general"),
-    ("2016", "0405", "special__primary"),
-    ("2016", "0607", "primary"),
-    ("2016", "1108", "general"),
-    ("2017", "0404", "special__primary"),
-    ("2017", "0606", "special__general"),
-    ("2017", "1003", "special__primary"),
+@pytest.mark.parametrize("year,date,election_type,county_directory", [
+    ("2014", "0603", "primary", ""),
+    ("2014", "1104", "general", ""),
+    ("2015", "0317", "special__primary", ""),
+    ("2015", "0519", "special__general", ""),
+    ("2016", "0405", "special__primary", "counties"),
+    ("2016", "0607", "primary", "counties"),
+    ("2016", "1108", "general", "counties"),
+    ("2017", "0404", "special__primary", ""),
+    ("2017", "0606", "special__general", ""),
+    ("2017", "1003", "special__primary", ""),
 ])
-def test_data(year, date, election_type):
+def test_data(year, date, election_type, county_directory):
     state_file = '{0}/{0}{1}__ca__{2}.csv'.format(
         year, date, election_type)
     state_data = pandas.read_csv(state_file, na_values='').fillna('')
     state_data = state_data[state_data.votes != 0]
 
-    for county_file in sorted(glob.glob('{0}/{0}{1}__ca__{2}__*__precinct.csv'.format(year, date, election_type))):
+    county_file_pattern = os.path.join(year, county_directory, f"{year}{date}__ca__{election_type}__*__precinct.csv")
+    for county_file in sorted(glob.glob(county_file_pattern)):
         county_data = pandas.read_csv(county_file, na_values='').fillna('')
 
         # Each county file should only contain a single county.
