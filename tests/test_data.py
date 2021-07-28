63d36f478ef31791a7e257733de83d110767aff4
Kenneth Hsu <kennethhsu@gmail.com>
2021-07-28 12:59:00 -0700

    Use subtests to show all county failures.
    Previously, the tests would stop after the first county failure.

--- tests/test_data.py
+++ tests/test_data.py
@@ -21,7 +21,7 @@ columns = ['candidate', 'county', 'district', 'office', 'votes']
     ("2017", "0606", "special__general", ""),
     ("2017", "1003", "special__primary", ""),
 ])
-def test_data(year, date, election_type, county_directory):
+def test_data(subtests, year, date, election_type, county_directory):
     state_file = '{0}/{0}{1}__ca__{2}.csv'.format(
         year, date, election_type)
     state_data = pandas.read_csv(state_file, na_values='').fillna('')
@@ -29,17 +29,18 @@ def test_data(year, date, election_type, county_directory):
 
     county_file_pattern = os.path.join(year, county_directory, f"{year}{date}__ca__{election_type}__*__precinct.csv")
     for county_file in sorted(glob.glob(county_file_pattern)):
-        county_data = pandas.read_csv(county_file, na_values='').fillna('')
+        with subtests.test(msg=f"{county_file}"):
+            county_data = pandas.read_csv(county_file, na_values='').fillna('')
 
-        # Each county file should only contain a single county.
-        assert len(county_data.drop_duplicates(
-            ['county']).county.values) == 1
+            # Each county file should only contain a single county.
+            assert len(county_data.drop_duplicates(
+                ['county']).county.values) == 1
 
-        county = county_data.drop_duplicates(['county']).county.values[0]
-        county_data = county_data[columns].groupby(
-            columns[:-1]).sum()
-        state_cmp = state_data[state_data.county == county][
-            columns].groupby(columns[: -1]).sum()
+            county = county_data.drop_duplicates(['county']).county.values[0]
+            county_data = county_data[columns].groupby(
+                columns[:-1]).sum()
+            state_cmp = state_data[state_data.county == county][
+                columns].groupby(columns[: -1]).sum()
 
-        assert state_cmp.to_dict()['votes'] == county_data.to_dict()[
-            'votes'], '%s failed' % county
+            assert state_cmp.to_dict()['votes'] == county_data.to_dict()[
+                'votes'], '%s failed' % county
